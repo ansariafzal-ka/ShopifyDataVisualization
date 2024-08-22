2a7d1cd6fa0265d65a302cf5024e7bd2688c21ba
ansariafzal <ansariafzal2112@gmail.com>
2024-08-22 15:07:02 +0530

    cors updated

--- api/server.js
+++ api/server.js
@@ -9,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://shopify-data-visualization.vercel.app",
+    ],
     methods: ["GET"],
     credentials: true,
   })
