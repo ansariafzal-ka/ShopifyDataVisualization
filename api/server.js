0c905a07f3a2a21f2c3f3736b80cc03cbe5e5d38
ansariafzal <ansariafzal2112@gmail.com>
2024-08-22 15:15:33 +0530

    server changed

--- api/server.js
+++ api/server.js
@@ -3,10 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDb = require("./src/config/database");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+connectDb();
 app.use(
   cors({
     origin: [
@@ -18,7 +15,9 @@ app.use(
   })
 );
 
-connectDb();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const analyticsRouter = require("./src/routes/analytics.routes");
 
 app.use("/api/v1/analytics", analyticsRouter);
